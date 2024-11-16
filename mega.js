9b6f31b4de6f44eec5b224714b788e3363bb2d78
lochanaanila <lochanaanila756@gmail.com>
2024-11-16 12:34:10 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'lochanaanila756@gmail.com',
-    password: 'Anila87869250227',
+    email: 'githubccsell@gmail.com',
+    password: 'hukannapky123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
